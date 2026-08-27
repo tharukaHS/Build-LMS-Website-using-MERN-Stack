81644a916261dc21e88d0a4211f95e097daa909d
tharukaHS <188729792+tharukaHS@users.noreply.github.com>
2026-08-27 10:08:23 +0530

    day2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/student/Home'
+import CoursesList from './pages/student/CoursesList'
+import CourseDetails from './pages/student/CourseDetails'
+import MyEnrollments from './pages/student/MyEnrollments'
+import Player from './pages/student/Player'
+import Loading from './components/student/Loading'
+import Educator from './pages/educator/Educator'
+import Dashboard from './pages/educator/Dashboard'
+import AddCourse from './pages/educator/AddCourse'
+import MyCourses from './pages/educator/MyCourses'
+import StudentsEnrolled from './pages/educator/StudentsEnrolled'
+
+
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CoursesList />} />
+        <Route path="/course-list/:input" element={<CoursesList />} />
+        <Route path="/course/:id" element={<CourseDetails />}/> 
+        <Route path="/my-enrollments" element={<MyEnrollments />} />
+        <Route path="/player/:courseId" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+        {/* Educator Routes. it go like files. folder has two folders inside it */}
+        <Route path="/educator" element={<Educator />} > 
+            <Route path="educator"  element={<Dashboard />} />
+            <Route path="add-course"  element={<AddCourse />} />
+            <Route path="my-courses"  element={<MyCourses />} />
+            <Route path="students-enrollments"  element={<StudentsEnrolled />} />
+
+
+
+        </Route>
+
+
+        </Routes>
+    </div>
   )
 }
 
