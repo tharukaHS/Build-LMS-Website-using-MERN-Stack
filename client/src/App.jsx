510e61808820e9b7828aef8223d2461693571303
tharukaHS <188729792+tharukaHS@users.noreply.github.com>
2026-09-01 13:42:58 +0530

    2026/09/01

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,101 @@
+
 import React from 'react'
-import { Route,Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+
 import Home from './pages/student/Home'
 import CoursesList from './pages/student/CoursesList'
 import CourseDetails from './pages/student/CourseDetails'
 import MyEnrollments from './pages/student/MyEnrollments'
 import Player from './pages/student/Player'
 import Loading from './components/student/Loading'
+
 import Educator from './pages/educator/Educator'
 import Dashboard from './pages/educator/Dashboard'
 import AddCourse from './pages/educator/AddCourse'
 import MyCourses from './pages/educator/MyCourses'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled'
 
-
+import Navbar from './components/student/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className="text-default min-h-screen bg-white">
+
+      <Navbar />
+
       <Routes>
+
+        {/* Student Routes */}
         <Route path="/" element={<Home />} />
-        <Route path="/course-list" element={<CoursesList />} />
-        <Route path="/course-list/:input" element={<CoursesList />} />
-        <Route path="/course/:id" element={<CourseDetails />}/> 
-        <Route path="/my-enrollments" element={<MyEnrollments />} />
-        <Route path="/player/:courseId" element={<Player />} />
-        <Route path="/loading/:path" element={<Loading />} />
-        {/* Educator Routes. it go like files. folder has two folders inside it */}
-        <Route path="/educator" element={<Educator />} > 
-            <Route path="educator"  element={<Dashboard />} />
-            <Route path="add-course"  element={<AddCourse />} />
-            <Route path="my-courses"  element={<MyCourses />} />
-            <Route path="students-enrollments"  element={<StudentsEnrolled />} />
+
+        <Route
+          path="/course-list"
+          element={<CoursesList />}
+        />
+
+        <Route
+          path="/course-list/:input"
+          element={<CoursesList />}
+        />
+
+        <Route
+          path="/course/:id"
+          element={<CourseDetails />}
+        />
+
+        <Route
+          path="/my-enrollments"
+          element={<MyEnrollments />}
+        />
+
+        <Route
+          path="/player/:courseId"
+          element={<Player />}
+        />
+
+        <Route
+          path="/loading/:path"
+          element={<Loading />}
+        />
 
 
+        {/* Educator Routes */}
+        <Route
+          path="/educator"
+          element={<Educator />}
+        >
+
+          {/* /educator */}
+          <Route
+            index
+            element={<Dashboard />}
+          />
+
+          {/* /educator/add-course */}
+          <Route
+            path="add-course"
+            element={<AddCourse />}
+          />
+
+          {/* /educator/my-courses */}
+          <Route
+            path="my-courses"
+            element={<MyCourses />}
+          />
+
+          {/* /educator/students-enrollments */}
+          <Route
+            path="students-enrollments"
+            element={<StudentsEnrolled />}
+          />
 
         </Route>
 
+      </Routes>
 
-        </Routes>
     </div>
   )
 }
 
 export default App
+
